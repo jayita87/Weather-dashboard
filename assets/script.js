69581955f11d429a5366a3fbf5933b3dc1f6e1b0
Jayita Chowdhury <jayitachowdhury@Amits-MacBook-Pro.local>
2022-08-20 21:14:13 -0700

    pushed

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+var apiKey = '6db38b3e698ee1b3978da9d45fa1fd4b';
+var city = 'phoenix';
+var units = 'imperial';
+var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=phoenix&units=imperial&appid=6db38b3e698ee1b3978da9d45fa1fd4b';
+
+var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + units +'&appid=' + apiKey;
+
+// https://api.openweathermap.org/data/2.5/forecast?q=phoenix&units=imperial&appid=6db38b3e698ee1b3978da9d45fa1fd4b
