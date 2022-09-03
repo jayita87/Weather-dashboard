e4b1b71b2fbce41ac8139b1d9b3189e3d36f4755
Jayita Chowdhury <jayitachowdhury@Amits-MacBook-Pro.local>
2022-09-02 21:18:03 -0700

    change some element

--- assets/script.js
+++ assets/script.js
@@ -11,84 +11,90 @@ var day5 = document.getElementById("Day5");
 function displayTime() {
     var rightNow = moment().format('MM/DD/YYYY');
 }
-  displayTime();
-  setInterval(displayTime, 1000);
+displayTime();
+setInterval(displayTime, 1000);
 
 // variable for API key
 var apiKey = 'bfb3545c6cc5780043f409b16e39f353';
 // variable for API url
 var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=phoenix&units=imperial&appid=bfb3545c6cc5780043f409b16e39f353';
 
-
-console.log(new Date(1660543200 * 1000));
-console.log(Math.floor(new Date().getTime() / 1000));
-
 var currentDate = new Date(moment().format());
-var futureDate = new Date();
-futureDate.setDate(currentDate.getDate() + 1);
-futureDate.setHours(5, 00, 00);
+var currentDatePlusOne = new Date();
+var currentDatePlusTwo = new Date();
+var currentDatePlusThree = new Date();
+var currentDatePlusFour = new Date();
+var currentDatePlusFive = new Date();
 
-console.log("Current Time::" + currentDate);
-console.log("Future Time::" + futureDate);
+currentDatePlusOne.setDate(currentDate.getDate() + 1);
+currentDatePlusTwo.setDate(currentDate.getDate() + 2);
+currentDatePlusThree.setDate(currentDate.getDate() + 3);
+currentDatePlusFour.setDate(currentDate.getDate() + 4);
+currentDatePlusFive.setDate(currentDate.getDate() + 5);
 
-console.log("FutureTime in Unix::" + Math.floor(futureDate.getTime() / 1000));
+currentDatePlusOne.setHours(5, 00, 00);
+currentDatePlusTwo.setHours(5, 00, 00);
+currentDatePlusThree.setHours(5, 00, 00);
+currentDatePlusFour.setHours(5, 00, 00);
+currentDatePlusFive.setHours(5, 00, 00);
+
+currentDatePlusOneInUnix = Math.floor(currentDatePlusOne.getTime() / 1000);
+currentDatePlusTwoInUnix = Math.floor(currentDatePlusTwo.getTime() / 1000);
+currentDatePlusThreeInUnix = Math.floor(currentDatePlusThree.getTime() / 1000);
+currentDatePlusFourInUnix = Math.floor(currentDatePlusFour.getTime() / 1000);
+currentDatePlusFiveInUnix = Math.floor(currentDatePlusFive.getTime() / 1000);
+
 
 searchBtn.addEventListener('click', function () {
     var cityName = textboxEl.value;
-    console.log(cityName);
     var cityArray = localStorage.getItem('cityname');
     currentWeather(cityName);
+    fiveDaysForcast(cityName);
     saveCityName(cityName);
     listSavedCity(cityName);
 }
 )
 function saveCityName(cityName) {
     var cityArray = JSON.parse(localStorage.getItem('cityName'));
-    console.log(cityArray);
 
-    if(cityArray == null){
+    if (cityArray == null) {
         var ctyArray = new Array();
         ctyArray.push(cityName);
         localStorage.setItem("cityName", JSON.stringify(ctyArray));
-    }else{
+    } else {
         cityArray.push(cityName);
         localStorage.setItem("cityName", JSON.stringify(cityArray));
     }
 }
 
-function listSaveCities(){    
-    var cityArray = JSON.parse(localStorage.getItem('cityName'));    
-    // var parentEl = document.getElementById("divTextbox");
-    
-    cityArray.forEach(listSavedCity);
-        // var childElement = document.createElement('div');
-        // childElement.classList.add("savedCities")
-        // childElement.textContent=a;        
-        // parentEl.appendChild(childElement);        
-    // )    
+function listSaveCities() {
+    var cityArray = JSON.parse(localStorage.getItem('cityName'));
+
+    cityArray.forEach(listSavedCity);  
 }
 
-function listSavedCity(cityName){
+function listSavedCity(cityName) {
     var parentEl = document.getElementById("divTextbox");
     var childElement = document.createElement('div');
     childElement.classList.add("savedCities")
-    childElement.textContent=cityName;        
-    parentEl.appendChild(childElement);        
+    childElement.textContent = cityName;
+    parentEl.appendChild(childElement);
 }
 
 listSaveCities();
 
-document.addEventListener( 'click', (event) =>
-{
-    if (event.target.className === 'savedCities')
-    {
+document.addEventListener('click', (event) => {
+    if (event.target.className === 'savedCities') {
         let cityName = event.target.textContent;
         currentWeather(cityName);
+        fiveDaysForcast(cityName);
     }
 });
 
 var currentDataAPI = "https://api.openweathermap.org/data/2.5/weather?q="
 var appIdAndUnit = "&units=imperial&appid=bfb3545c6cc5780043f409b16e39f353"
+
+var forecastAPI="https://api.openweathermap.org/data/2.5/forecast?q="
 
 function currentWeather(cityName) {
     fetch(currentDataAPI + cityName + appIdAndUnit)
@@ -96,86 +102,118 @@ function currentWeather(cityName) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-
-
             var child = document.createElement('div');
 
             forecastEl.appendChild(child);
             forecastEl.innerHTML =
                 `<div>   
-   <p>Temp: ${data.main.temp}f</p>
-    <p>Highs: ${data.main.temp_max}f. <br> Lows : ${data.main.temp_min}f</p>
-    <p>Humidity: ${data.main.humidity}</p>
-    <p>feels like: ${data.main.feels_like}f</p>
+     <img src="http://openweathermap.org/img/wn//${data.weather[0].icon}@2x.png" alt="weather icon" class="w-icon"> 
+     <p>${window.moment(data.dt * 1000).format('L')}</p>
+    
+     <p> ${data.weather[0].description}</p>
+     <p>Wind speed: ${data.wind.speed}MPH</p>
+     
+   <p>Temp: ${data.main.temp}F</p>
+    <p>Humidity: ${data.main.humidity}%</p>
 </div>`
         });
 }
-function fiveDaysForcast() {
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=phoenix&units=imperial&appid=bfb3545c6cc5780043f409b16e39f353')
+
+function fiveDaysForcast(cityName) {
+
+    fetch(forecastAPI + cityName + appIdAndUnit)
         .then(function (response) {
             return response.json();
         })
-        .then(function (data) {
-            console.log(data);
+        .then(function (data) {        
             var fivedays = document.createElement('div');
             day1.appendChild(fivedays);
-            day1.innerHTML =
-                `<div>   
-                   <p> ${data.list[0].weather[0].description}</p>
-                <p>Date: ${data.list[0].dt}</p>  
-     <p>Temp: ${data.list[0].main.temp}f</p>
-    <p>Feels like: ${data.list[0].main.feels_like}f</p>
-    <p>Humidity: ${data.list[0].main.humidity}%</p>
-    <p>Lows: ${data.list[0].main.temp_min}f</p>
-    <p>Highs: ${data.list[0].main.temp_max}f</p>
-</div>`
-;
-day2.appendChild(fivedays);
-            day2.innerHTML =
-                `<div>   
-                <p>${data.list[1].weather[0].icon,data.list[1].weather[0].description}</p>
-                <p>Date: ${data.list[1].dt}</p>  
-     <p>Temp: ${data.list[1].main.temp}f</p>
-    <p>Feels like: ${data.list[1].main.feels_like}f</p>
-    <p>Humidity: ${data.list[1].main.humidity}%</p>
-    <p>Lows: ${data.list[1].main.temp_min}f</p>
-    <p>Highs: ${data.list[1].main.temp_max}f</p>
-</div>`
-day3.appendChild(fivedays);
-            day3.innerHTML =
-                `<div>  
-                 <p>${data.list[2].weather[0].icon,data.list[2].weather[0].description}</p>
-                <p>Date: ${data.list[2].dt}</p>  
-     <p>Temp: ${data.list[2].main.temp}f</p>
-    <p>Feels like: ${data.list[2].main.feels_like}f</p>
-    <p>Humidity: ${data.list[2].main.humidity}%</p>
-    <p>Lows: ${data.list[2].main.temp_min}f</p>
-    <p>Highs: ${data.list[2].main.temp_max}f</p>
-</div>`
-day4.appendChild(fivedays);
-            day4.innerHTML =
-                `<div>   
-                <p>${data.list[3].weather[0].icon,data.list[3].weather[0].description}</p>
-                <p>Date: ${data.list[3].dt}</p>  
-     <p>Temp: ${data.list[3].main.temp}f</p>
-    <p>Feels like: ${data.list[3].main.feels_like}f</p>
-    <p>Humidity: ${data.list[3].main.humidity}%</p>
-    <p>Lows: ${data.list[3].main.temp_min}f</p>
-    <p>Highs: ${data.list[3].main.temp_max}f</p>
-</div>`
-day5.appendChild(fivedays);
-            day5.innerHTML =
-                `<div> 
-                <p>${data.list[4].weather[0].icon,data.list[4].weather[0].description}</p>
-                <p>Date: ${data.list[4].dt}</p>    
-     <p>Temp: ${data.list[4].main.temp}f</p>
-    <p>Feels like: ${data.list[4].main.feels_like}f</p>
-    <p>Humidity: ${data.list[4].main.humidity}%</p>
-    <p>Lows: ${data.list[4].main.temp_min}f</p>
-    <p>Highs: ${data.list[4].main.temp_max}f</p>
-</div>`
+            data.list.forEach(
+                element => {
+                    if (element.dt == currentDatePlusOneInUnix) {
+                        // day1.innerHTML = addElementsToView(day1, element)
+                        day1.innerHTML =
+                            `<div>                           
+                            <p>${window.moment(element.dt_txt).format('L')}</p>
+                        <img src="http://openweathermap.org/img/wn//${element.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">                        
+                        <p> ${element.weather[0].description}</p>                                                
+                    <p>Temp: ${element.main.temp}F</p>
+                    <p>Wind speed: ${element.wind.speed}MPH</p>
+                    <p>Humidity: ${element.main.humidity}%</p>
+                    </div>`;
+                    }
+
+                    if (element.dt == currentDatePlusTwoInUnix) {
+                        // day1.innerHTML = addElementsToView(day1, element)
+                        day2.innerHTML =
+                            `<div>                           
+                            <p>${window.moment(element.dt_txt).format('L')}</p>
+                        <img src="http://openweathermap.org/img/wn//${element.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                        
+                        <p> ${element.weather[0].description}</p>                                                
+                    <p>Temp: ${element.main.temp}F</p>
+                    <p>Wind speed: ${element.wind.speed}MPH</p>
+                    <p>Humidity: ${element.main.humidity}%</p>
+                    </div>`;
+                    }
+
+                    if (element.dt == currentDatePlusThreeInUnix) {
+                        // day1.innerHTML = addElementsToView(day1, element)
+                        day3.innerHTML =
+                            `<div>                           
+                            <p>${window.moment(element.dt_txt).format('L')}</p>
+                        <img src="http://openweathermap.org/img/wn//${element.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                        
+                        <p> ${element.weather[0].description}</p>                                                
+                    <p>Temp: ${element.main.temp}F</p>
+                    <p>Wind speed: ${element.wind.speed}MPH</p>
+                    <p>Humidity: ${element.main.humidity}%</p>
+                    </div>`;
+                    }
+
+                    if (element.dt == currentDatePlusFourInUnix) {
+                        // day1.innerHTML = addElementsToView(day1, element)
+                        day4.innerHTML =
+                            `<div>                           
+                            <p>${window.moment(element.dt_txt).format('L')}</p>
+                        <img src="http://openweathermap.org/img/wn//${element.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                        
+                        <p> ${element.weather[0].description}</p>                                                
+                    <p>Temp: ${element.main.temp}F</p>
+                    <p>Wind speed: ${element.wind.speed}MPH</p>
+                    <p>Humidity: ${element.main.humidity}%</p>
+                    </div>`;
+                    }
+
+
+                    if (element.dt == currentDatePlusFiveInUnix) {
+                        // day1.innerHTML = addElementsToView(day1, element)
+                        day5.innerHTML =
+                            `<div>                           
+                            <p>${window.moment(element.dt_txt).format('L')}</p>
+                        <img src="http://openweathermap.org/img/wn//${element.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+                        
+                        <p> ${element.weather[0].description}</p>                                                
+                    <p>Temp: ${element.main.temp}F</p>
+                    <p>Wind speed: ${element.wind.speed}MPH</p>
+                    <p>Humidity: ${element.main.humidity}%</p>
+                    </div>`;
+                    }
+
+
+                }
+            );
         })
 
 }
-fiveDaysForcast();
+
+function addElementsToView(day1, dataMonday) {
+    return
+    `<div>   
+    <img src="http://openweathermap.org/img/wn//${dataMonday.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+       <p> ${dataMonday.weather[0].description}</p>
+    <p>Date: ${dataMonday.dt_txt}</p>  
+<p>Temp: ${dataMonday.main.temp}f</p>
+<p>Humidity: ${dataMonday.main.humidity}%</p>
+</div>`;
+}
